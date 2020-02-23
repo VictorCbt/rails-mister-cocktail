3dd6437a27d5f686a7c1a4e8a0bb71a35184061a
Victor Chabot <victor.chabot@gmail.com>
2020-02-23 15:05:59 +0100

    exercice done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  has_many :reviews
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_many :doses, dependant: :destroy
-  validates :names, presence: true, uniqueness: true
+  has_many :reviews
+  validates :name, presence: true, uniqueness: true
 end
